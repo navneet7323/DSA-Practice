e17d16844db0e47fd315545103264e3f48403cf6
Navneet Kumar <navneetsingh70mth@gmail.com>
2026-06-21 13:05:23 +0530

    psuh

--- string/Maximum Number of Words.js
+++ string/Maximum Number of Words.js
@@ -1,12 +1,12 @@
-let sentences = [
+let sentence = [
   "alice and bob love leetcode",
   "i think so too",
   "this is great thanks very much",
 ];
-let maxWord = sentences[0];
-for (let i = 0; i < sentences.length; i++) {
-  if (sentences[i] > maxWord) {
-    maxWord = sentences[i];
+let maxWord = sentence[0];
+for (let i = 0; i < sentence.length; i++) {
+  if (sentence[i] > maxWord) {
+    maxWord = sentence[i];
   }
 }
 console.log(`Maximum number of word = ${maxWord}`);

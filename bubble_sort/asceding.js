e74f0db02ee8f89538f3ca23a022fad6ff3abaee
Navneet Kumar <navneetsingh70mth@gmail.com>
2026-06-23 10:37:23 +0530

    add

--- bubble_sort/asceding.js
+++ bubble_sort/asceding.js
@@ -3,9 +3,9 @@ let arr = [5, 4, 3, 2, 1];
 for (let i = 0; i < arr.length - 1; i++) {
   for (let j = 0; j < arr.length - 1 - i; j++) {
     if (arr[j] > arr[j + 1]) {
-      let temp = arr[j];
+      let swap = arr[j];
       arr[j] = arr[j + 1];
-      arr[j + 1] = temp;
+      arr[j + 1] = swap;
     }
   }
 }

844932a14ff10d2a2d1186155e154a8479bd2203
Navneet Kumar <navneetsingh70mth@gmail.com>
2026-08-08 21:17:02 +0530

    Searching

--- searching/First_and_Last_Occurrences.js
+++ searching/First_and_Last_Occurrences.js
@@ -1,17 +1,51 @@
-// let arr = [10, 20, 30, 40, 50, 50, 60];
+let arr = [2, 4, 6, 8, 8, 8, 8, 11, 13];
 
-// let first = -1;
-// let last = -1;
+let target = 8;
 
-// for (let i = 0; i < arr.length; i++) {
-//   if (arr[i] === 50) {
-//     if (first === -1) {
-//       first = i;
-//     }
-//     last = i;
-//   }
-// }
-// console.log(first);
-// console.log(last);
+let start = 0;
+let end = arr.length - 1;
+
+let first = -1;
+
+// Find first occurrence
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === target) {
+    first = mid;
+    end = mid - 1; // search left
+  } 
+  else if (arr[mid] < target) {
+    start = mid + 1;
+  } 
+  else {
+    end = mid - 1;
+  }
+}
+
+console.log(`First occurrence of ${target} is index ${first}`);
 
 
+// RESET start and end
+start = 0;
+end = arr.length - 1;
+
+let last = -1;
+
+// Find last occurrence
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === target) {
+    last = mid;
+    start = mid + 1; // search right
+  } 
+  else if (arr[mid] < target) {
+    start = mid + 1;
+  } 
+  else {
+    end = mid - 1;
+  }
+}
+
+console.log(`Last occurrence of ${target} is index ${last}`);
